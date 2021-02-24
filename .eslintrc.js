957c2210d6e49ef024eed4962ca7abb3b0b17915
TatianaKostereva <tatjana.kostereva@yandex.ru>
2021-02-24 22:59:07 +0300

    add

---
+++ .eslintrc.js
@@ -0,0 +1,52 @@
+const path = require('path');
+
+module.exports = {
+  parser: 'vue-eslint-parser',
+  globals: {
+    window: true,
+    document: false
+  },
+  extends: [
+    'eslint:recommended',
+    'airbnb-base',
+    'plugin:vue/recommended',
+    '@vue/typescript'
+  ],
+  settings: {
+    'import/resolver': {
+      webpack: {
+        config: {
+          resolve: {
+            alias: {
+              '~': path.join(__dirname, 'source'),
+            },
+            extensions: ['.js', '.jsx', '.ts', '.tsx', '.mjs'],
+          },
+        },
+      }
+    },
+  },
+  rules: {
+    'linebreak-style': 0,
+    'no-new': 0,
+    'no-bitwise': [
+      'error', 
+      { 
+        'allow': ['~']
+      }
+    ],
+    'import/no-unresolved': 0,
+    "import/prefer-default-export": "off",
+    'import/no-extraneous-dependencies': 0,
+    'no-param-reassign': 0,
+    'no-irregular-whitespace': 0,
+    'class-methods-use-this': 0,
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        ts: 'never',
+      }
+    ],
+  }
+}
